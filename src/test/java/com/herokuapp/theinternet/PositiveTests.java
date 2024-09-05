2078f79e6941a57d01b42ad347ed65cd2a1337e6
QA Bob <rlsmall@cox.net>
2024-09-05 12:07:29 -0700

    added pom selenium 4.20.0

--- src/test/java/com/herokuapp/theinternet/PositiveTests.java
+++ src/test/java/com/herokuapp/theinternet/PositiveTests.java
@@ -2,25 +2,19 @@ package com.herokuapp.theinternet;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 public class PositiveTests {
 
     @Test
     public void loginTest() {
-        openPage();
-    }
-
-    @AfterTest
-    public void tearDown(WebDriver driver){
-        driver.quit();
-    }
-
-    private static void openPage() {
+        System.out.println("Running openPage test");
         WebDriver driver = new ChromeDriver();  //TODO:Set this in runtime from the Google Analytics
         String url = "https://schwab.com";  //TODO: Set this in runtime from Jenkins or the CLI
         driver.get(url);
         driver.manage().window().maximize();
+        driver.quit();
+        System.out.println("Closed browser");
+        System.out.println("Completed openPage test");
     }
 }
